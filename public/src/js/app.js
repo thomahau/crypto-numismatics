cb55ca697ebf67d89675328b242a5319855826f7
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2020-07-12 02:31:24 +0200

    check data

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -13,6 +13,7 @@ const App = {
 
       App.getTickerData(currency)
         .then(data => {
+          console.log('data', data);
           tickerData = data;
           App.UI.renderPortfolio();
           App.handleLogout();
@@ -123,7 +124,7 @@ const App = {
     // returns current ticker data for all the cryptocurrencies tracked by nomics.com
     const url = `tickers/${currency}`;
     const res = await fetch(url);
-    console.log('res', res);
+    // console.log('res', res);
     // console.log('res.json', res.json());
     return res.json();
   //   return fetch(url).then(res => {
